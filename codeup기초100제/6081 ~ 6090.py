af4bbbdc29c4e72f5a7d0933b96cb70b8bcee2da
wooseok <80329046+Low1ife@users.noreply.github.com>
2022-04-28 17:21:08 +0900

    Update 6081 ~ 6090.py

--- codeup기초100제/6081 ~ 6090.py
+++ codeup기초100제/6081 ~ 6090.py
@@ -1,1 +1,29 @@
-#6081
+#6081 16진수 구구단
+# a = int(input() , 16)
+
+# for i in range( 1 , 16 ):
+#     print('%X*%X=%X' %(a,i,a*i))
+
+# 3 6 9 game
+
+# a = int(input())
+
+# for i in range( 1 , a + 1):
+#     if i % 10 == 3:
+#         print('X')
+#     elif i % 10 == 6:
+#         print('X')
+#     elif i % 10 == 9:
+#         print('X')
+#     else :
+#         print(i)
+
+# rgb 경우의 수
+# r,g,b = map(int , input().split())
+
+# for i in range(r):
+#     for j in range(g):
+#         for k in range(b):
+#             print(i ,j, k ,sep=' ')
+
+# print(r*g*b)
