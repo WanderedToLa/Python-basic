0c9110d222d44de278f8de855ea640dca07eafba
wooseok <87642287+WanderedToLa@users.noreply.github.com>
2022-05-10 17:30:39 +0900

    Update 6097설탕과자뽑기.py

--- codeup기초100제/6097설탕과자뽑기.py
+++ codeup기초100제/6097설탕과자뽑기.py
@@ -12,10 +12,10 @@ for i in range(n):
     for j in range(l):
         if b == 0:
             for k in range(l):
-                d[x][k] = 1
+                d[x -1][y - 1 + k] = 1 # + k와 m은 좌표를 한칸씩 당기고 반복문만큼 길이를 증가시킴
         elif b == 1:
             for m in range(l):
-                d[m][y] = 1
+                d[x - 1 + m][y - 1] = 1
             
 for i in range(w):
     for j in range(h):
